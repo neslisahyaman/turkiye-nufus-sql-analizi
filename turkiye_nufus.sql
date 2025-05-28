e92900772d9824ee3fc801427bf91e58aad505a8
Neslişah <neslisahyaman05@gmail.com>
2025-05-28 16:04:27 +0300

    Update turkiye_nufus.sql

--- turkiye_nufus.sql
+++ turkiye_nufus.sql
@@ -1,11 +1,11 @@
 
--- Türkiye Nüfus Verisi Tablosu Oluşturma
+
 CREATE TABLE turkiye_nufus (
     il VARCHAR(50),
     nufus INT
 );
 
--- Veri Ekleme
+
 INSERT INTO turkiye_nufus (il, nufus) VALUES
 ('İstanbul', 15840900),
 ('Ankara', 5663322),
@@ -18,12 +18,12 @@ INSERT INTO turkiye_nufus (il, nufus) VALUES
 ('Gaziantep', 2131191),
 ('Kocaeli', 2046442);
 
--- En kalabalık il
+
 SELECT * FROM turkiye_nufus
 ORDER BY nufus DESC
 LIMIT 1;
 
--- Nüfusu 3 milyon üzeri olan iller
+
 SELECT * FROM turkiye_nufus
 WHERE nufus > 3000000;
 
